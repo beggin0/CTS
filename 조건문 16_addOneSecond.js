5dc8a16f10850040799867711a85eb4c077f3c3a
beggin0 <beggin823@gmail.com>
2022-12-09 23:43:30 +0900

    2022.12.09 16번 해결

--- 조건문 16_addOneSecond.js
+++ 조건문 16_addOneSecond.js
@@ -34,10 +34,25 @@ function addOneSecond(hour, minute, second) {
     if (second === 59) {
         minute += 1;
         second = 0;
-      } else {
+    } else {
         second += 1;
     }
+    if (minute = 60) {
+        hour += 1;
+        minute === 0;
+    }
+    if (hour === 24) {
+        hour = 0;
+    }
+    return `1초 뒤에 ${hour}시 ${minute}분 ${second}초 입니다`
 }
 
 // 나올 메시지 1초 + 했을경우 분, 시간 바뀌는거 
 // 시, 분, 초 24 60 60 될 때 0 이되는 함수를 하나 구성해도 좋을듯하다.
+// 초
+// 분
+// 시
+
+// 통과가 안되 리턴값을 지정 안해줬고, 음 분이랑 시간이 따로 놀아서 안되네
+// === 는 true false를 답하고, = 는 할당을 뜻해 잘 생각해서 쓰자.
+// 문자열에 변수 사용하기(템플릿 리터럴) ``
