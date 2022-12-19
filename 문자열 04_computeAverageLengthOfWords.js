0cfdeaa5391f189f785e069b3fa935ed8af26fab
beggin0 <beggin823@gmail.com>
2022-12-19 23:42:41 +0900

    2022.12.19 4번

--- 문자열 04_computeAverageLengthOfWords.js
+++ 문자열 04_computeAverageLengthOfWords.js
@@ -21,5 +21,9 @@ number 타입을 리턴해야 합니다.
 // TODO: 여기에 코드를 작성합니다.
 
 function computeAverageLengthOfWords(word1, word2) {
-
+    if (word1.length > 0 || word2.length > 0) {
+        return (word1.length + word2.length) / 2
+    }
 }
+
+// word1, word2가 한글자 이상이라면 둘이 합해서 나누면 된다. 
